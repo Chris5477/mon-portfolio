64b70179fb1111503e8e05ce7c797793127a61c0
Chris5477 <petitcolaschristopher@gmail.com>
2022-05-27 20:57:07 +0200

    fix syntax error

--- src/utils/downloadFile.js
+++ src/utils/downloadFile.js
@@ -3,4 +3,3 @@ import fileSaver from "file-saver";
 export const saveFile = () => {
 	fileSaver.saveAs(process.env.PUBLIC_URL + "/assets/CVChristopherPetitcolas.pdf", "CVChristopherPetitcolas.pdf");
 };
-a
